161151f88482a4ba1955fa661b7ec9c0c81b3995
Altaf Quadri <altafmquadri@gmail.com>
2020-02-05 23:26:51 -0500

    comments added

--- bst.js
+++ bst.js
@@ -11,16 +11,21 @@ class BinarySearchTree {
         this.root = null
     }
     insert(val) {
+        //declare new node
         let newNode = new Node(val)
         let current = this.root
+
+        //if it's an empty tree then simply add
         if(!this.root) {
             this.root = newNode
             return this
         } 
         while (true) {
+            // check to avoid duplicated entries
             if (newNode.val === current.val) return undefined
             if (val < current.val) {
                 if (current.left === null) {
+                    //traverse down the tree by setting the child to be the new current
                     current.left = newNode
                     return this
                 } 
@@ -39,6 +44,7 @@ class BinarySearchTree {
     find(val) {
         let current = this.root
         const findVal = (c) => {
+            //base cases to break out of the recursive calls
             if (c === null) return false
             if (c.val === val) {
                 return true
