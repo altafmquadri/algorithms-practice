a3d252ba14a3dc3c2dc849c8277c42bc1c071d8f
Altaf Quadri <altafmquadri@gmail.com>
2020-02-14 16:25:29 -0500

    added bfs

--- bst.js
+++ bst.js
@@ -16,10 +16,10 @@ class BinarySearchTree {
         let current = this.root
 
         //if it's an empty tree then simply add
-        if(!this.root) {
+        if (!this.root) {
             this.root = newNode
             return this
-        } 
+        }
         while (true) {
             // check to avoid duplicated entries
             if (newNode.val === current.val) return undefined
@@ -28,10 +28,10 @@ class BinarySearchTree {
                     //traverse down the tree by setting the child to be the new current
                     current.left = newNode
                     return this
-                } 
+                }
                 current = current.left
-            } 
-            else if (newNode.val > current.val ) {
+            }
+            else if (newNode.val > current.val) {
                 if (current.right === null) {
                     current.right = newNode
                     return this
@@ -52,7 +52,7 @@ class BinarySearchTree {
             if (val < c.val) {
                 c = c.left
                 return findVal(c)
-            } 
+            }
             if (val > c.val) {
                 c = c.right
                 return findVal(c)
@@ -60,11 +60,39 @@ class BinarySearchTree {
         }
         return findVal(current)
     }
+
+    BFS = () => {
+        if (!this.root) return null
+        //create a queue to process 
+        let queue = [this.root]
+        let visited = []
+        let current
+        while (queue.length) {
+            current = queue.shift()
+            //keep checking until there are no more children
+            if (current.left !== null) queue.push(current.left)
+            if (current.right !== null) queue.push(current.right)
+            //add the value of the node to our visited array
+            visited.push(current.val)
+        }
+        return visited
+    }
 } //end BST
 
 const bst = new BinarySearchTree()
+// bst.insert(10)
+// bst.insert(6)
+// bst.insert(15)
+// bst.insert(3)
+// bst.insert(8)
+// bst.insert(20)
+
 bst.insert(30)
 bst.insert(20)
 bst.insert(60)
+bst.insert(10)
+bst.insert(40)
+
+console.log(bst.BFS()) //(5) [30, 20, 60, 10, 40]
 
 
